cdcf0b2fe2e2a921c57e35035f77f3aa89c87f3f
Your Name <141324213+MorsiYoucef@users.noreply.github.com>
2024-11-04 20:34:44 +0100

    new commit

--- lesson0/myproject/posts/urls.py
+++ lesson0/myproject/posts/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path,include
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.post_list),
+    path('', views.post_list, name='list'),
+    path('<slug:slug>', views.post_page, name='page'),
+
+
 ]
