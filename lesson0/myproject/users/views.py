c76953988cc45a33f9d896dc655f8d96ecef6a18
Your Name <141324213+MorsiYoucef@users.noreply.github.com>
2024-11-09 18:45:14 +0100

    new commit

--- lesson0/myproject/users/views.py
+++ lesson0/myproject/users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
@@ -6,8 +7,20 @@ def register_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect("posts:list")
+            user = form.save()
+            login(request, user)  # Log the user in after registration
+            return redirect('home')  # Change 'home' to the URL you want after login
     else:
         form = UserCreationForm()
-    return render(request, "users/register.html", {"form": form})
+    return render(request, 'users/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            #login
+            return redirect("posts:list")
+        
+    else:
+        form = AuthenticationForm()
+        return render(request, "users/login.html", {"form": form})
